0e50b3fd5d15ad5afdee1c458d9c27de4c89b8b0
Abdumalikov Sardor <sardorlearnjs@gmail.com>
2024-05-09 23:26:29 +0700

    bag with my_tags

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from catalog.models import Product
+
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home_temp.html', context)
+
+
+def home_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'home_product.html', context)
+
 
 def contacts(request):
     if request.method == 'POST':
